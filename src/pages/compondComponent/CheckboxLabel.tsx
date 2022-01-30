ceef9a242e617315e761e343249898f2b8d7324f
Prodipta Banerjee <prodiptaj@live.com>
2022-01-30 17:46:55 +0600

    Chor ESlint setup

--- src/pages/compondComponent/CheckboxLabel.tsx
+++ src/pages/compondComponent/CheckboxLabel.tsx
@@ -1,5 +1,6 @@
 function CheckboxLabel({ children, checked, setChecked }: any) {
   return (
+    // eslint-disable-next-line jsx-a11y/label-has-associated-control
     <label>
       <button type="button" onClick={() => setChecked(!checked)}>
         {children}
